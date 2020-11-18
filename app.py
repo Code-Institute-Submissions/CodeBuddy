bdc01b9b1edd1aae44e1d1b82f2b96ca50474438
jyfoo213 <jyfoo213@gmail.com>
2020-11-18 10:55:17 +0000

    switch debug to false

--- app.py
+++ app.py
@@ -329,4 +329,4 @@ def confirm_delete_comment(thread_id, comment_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
